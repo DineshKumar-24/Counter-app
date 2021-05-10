e62c5f476718bc701b267a9e9ab322c1640095f9
DineshKumar-24 <dancerdinesh111@gmail.com>
2021-05-10 13:30:10 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'counter-app';
+  count:number=0;
+  handleIncrease = () =>
+  {
+   
+    this.count+=1;
+  };
+  handleDecrease = () =>
+    {
+      this.count-=1;
+    };
+    handleReset=()=>
+    {
+      this.count=0;
+    };
+}
